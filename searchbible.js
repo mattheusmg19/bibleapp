bd7c284d6d289d0621e377629f32dfc057e56c82
Matheus <matheusmesquita690@gmail.com>
2025-05-03 17:36:01 -0300

    adicionando input de pesquisa na biblia

---
+++ searchbible.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchButton = document.querySelector(".search");
+    const searchInput = document.getElementById("search-input");
+  
+    searchButton.addEventListener("click", (event) => {
+      searchInput.style.display = "block";
+      searchInput.focus();
+      event.stopPropagation();
+    });
+  
+    document.addEventListener("click", (event) => {
+      if (searchInput.style.display === "block" && !searchButton.contains(event.target) && !searchInput.contains(event.target)) {
+        searchInput.style.display = "none";
+        searchInput.value = ""; 
+        carregarLivros();
+      }
+    });
+  
+    searchInput.addEventListener("input", filtrarLivros);
+  });
+  
+  function removerAcentos(texto) {
+    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+  }
+  
+  function filtrarLivros() {
+    const termoBusca = removerAcentos(document.getElementById("search-input").value);
+    const container = document.getElementById("tela-livros");
+    container.innerHTML = "";
+  
+    biblia.forEach((livro, index) => {
+      if (removerAcentos(livro.name).includes(termoBusca)) {
+        const div = document.createElement("div");
+        div.className = "livro";
+        
+        const idDiv = document.createElement("div");
+        idDiv.className = "livro-id";
+        idDiv.textContent = livro.id.toUpperCase();
+        
+        const infoDiv = document.createElement("div");
+        infoDiv.className = "livro-info";
+        infoDiv.innerHTML = `<h2>${livro.name}</h2><small>Capítulos: ${livro.chapters.length}</small>`;
+        
+        div.appendChild(idDiv);
+        div.appendChild(infoDiv);
+        div.onclick = () => carregarCapitulos(index);
+        container.appendChild(div);
+      }
+    });
+  }
+  
